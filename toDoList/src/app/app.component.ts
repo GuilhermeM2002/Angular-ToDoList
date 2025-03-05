a89a67c865c17ffcabb6d1f989fc2fe6dd80c3d2
GuilhermeM2002 <guilherme.infor2@gmail.com>
2025-03-05 20:10:41 -0300

    add: category menu

--- toDoList/src/app/app.component.ts
+++ toDoList/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
+import { CategoryComponent } from "./components/category/category.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, CategoryComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ toDoList/src/app/components/category/category.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-category',
+  imports: [],
+  
+templateUrl: './category.component.html',
+  styleUrl: './category.component.scss'
+})
+export class CategoryComponent {
+  @Input() title!: string;
+  @Input() color: string = 'white';
+}
